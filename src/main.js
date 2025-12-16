02498974891fd99f48aa7bcaec60395e41d6243e
LimeJacob <stopbreakingplease@gmail.com>
2025-12-16 16:29:35 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -188,3 +188,42 @@ hiddenCards.forEach((el) => cardObserver.observe(el));
 
 const hiddenCardsAlt = document.querySelectorAll('.c-hidden-alt');
 hiddenCardsAlt.forEach((el) => cardObserverAlt.observe(el));
+
+const CDP = require('chrome-remote-interface');
+
+setTimeout(() => {
+    CDP(async (client) => {
+        const {Network, Page, Runtime} = client;
+
+        const requests = new Set(); // <---------- HERE
+
+        Network.requestWillBeSent(({requestId, request}) => {
+            if(request.url.indexOf("ct2/results/rpc") != -1){
+                console.log(`REQ [${requestId}] ${request.method} ${request.url} \n`);
+
+                requests.add(requestId); // <---------- HERE
+
+            }
+        });
+        Network.loadingFinished(async ({requestId}) => {
+
+            if (requests.has(requestId)) { // <---------- HERE
+
+                const {body, base64Encoded} = await Network.getResponseBody({requestId});
+                console.log(`RES [${requestId}] body: ${body} \n`);
+            }
+        });
+        try {
+            await Promise.all([Network.enable(), Page.enable()]);
+            await Page.navigate({url: 'https://clinicaltrials.gov/ct2/results?cond=Parents&term=&cntry1=&state1=&Search=Search&recrs=a#wrapper'});
+            await Page.loadEventFired();
+            await Runtime.evaluate({
+                expression: `document.querySelector('.paginate_button.next').click()`
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    }).on('error', (err) => {
+        console.error(err);
+    });
+}, 1000);
